6e2cf66e7a0a537db5043579ca02c16ae1fb5c05
Haley Myracle <myracle.haley@hcschoolstn.org>
2021-04-08 14:43:35 -0500

    Update main.js

--- SHHS-Band-Website-Awasome/main.js
+++ SHHS-Band-Website-Awasome/main.js
@@ -1,4 +1,3 @@
-alert("Become Band MEMBER"
 
 var day_of_week = new Array('Sun','Mon','Tue','Wed','Thu','Fri','Sat');
 var month_of_year = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
@@ -105,3 +104,4 @@ cal += '</TD></TR></TABLE></TABLE>';
 document.write(cal);
             
 //  End -->
+
