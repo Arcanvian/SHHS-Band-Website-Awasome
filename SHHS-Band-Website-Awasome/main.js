369e7e9f8d4914a59419bd49ff3a54846eaad5a0
Haley Myracle <myracle.haley@hcschoolstn.org>
2021-04-12 14:07:47 -0500

    yessss

--- SHHS-Band-Website-Awasome/main.js
+++ SHHS-Band-Website-Awasome/main.js
@@ -1,107 +1,353 @@
+!function() {
 
-var day_of_week = new Array('Sun','Mon','Tue','Wed','Thu','Fri','Sat');
-var month_of_year = new Array('January','February','March','April','May','June','July','August','September','October','November','December');
-            
-//  DECLARE AND INITIALIZE VARIABLES
-var Calendar = new Date();
-            
-var year = Calendar.getFullYear();     // Returns year
-var month = Calendar.getMonth();    // Returns month (0-11)
-var today = Calendar.getDate();    // Returns day (1-31)
-var weekday = Calendar.getDay();    // Returns day (1-31)
-            
-var DAYS_OF_WEEK = 7;    // "constant" for number of days in a week
-var DAYS_OF_MONTH = 31;    // "constant" for number of days in a month
-var cal;    // Used for printing
-            
-Calendar.setDate(1);    // Start the calendar day at '1'
-Calendar.setMonth(month);    // Start the calendar month at now
-            
-            
-/* VARIABLES FOR FORMATTING
-NOTE: You can format the 'BORDER', 'BGCOLOR', 'CELLPADDING', 'BORDERCOLOR'
-tags to customize your caledanr's look. */
-            
-var TR_start = '<TR>';
-var TR_end = '</TR>';
-var highlight_start = '<TD WIDTH="30"><TABLE CELLSPACING=0 BORDER=1 BGCOLOR=DEDEFF BORDERCOLOR=CCCCCC><TR><TD WIDTH=20><B><CENTER>';
-var highlight_end   = '</CENTER></TD></TR></TABLE></B>';
-var TD_start = '<TD WIDTH="30"><CENTER>';
-var TD_end = '</CENTER></TD>';
-            
-/* BEGIN CODE FOR CALENDAR
-NOTE: You can format the 'BORDER', 'BGCOLOR', 'CELLPADDING', 'BORDERCOLOR'
-tags to customize your calendar's look.*/
-            
-cal =  '<TABLE BORDER=1 CELLSPACING=0 CELLPADDING=0 BORDERCOLOR=BBBBBB><TR><TD>';
-cal += '<TABLE BORDER=0 CELLSPACING=0 CELLPADDING=2>' + TR_start;
-cal += '<TD COLSPAN="' + DAYS_OF_WEEK + '" BGCOLOR="#EFEFEF"><CENTER><B>';
-cal += month_of_year[month]  + '   ' + year + '</B>' + TD_end + TR_end;
-cal += TR_start;
-            
-//   DO NOT EDIT BELOW THIS POINT  //
-            
-// LOOPS FOR EACH DAY OF WEEK
-for(index=0; index < DAYS_OF_WEEK; index++)
-{
-            
-// BOLD TODAY'S DAY OF WEEK
-if(weekday == index)
-cal += TD_start + '<B>' + day_of_week[index] + '</B>' + TD_end;
-            
-// PRINTS DAY
-else
-cal += TD_start + day_of_week[index] + TD_end;
-}
-            
-cal += TD_end + TR_end;
-cal += TR_start;
-            
-// FILL IN BLANK GAPS UNTIL TODAY'S DAY
-for(index=0; index < Calendar.getDay(); index++)
-cal += TD_start + '  ' + TD_end;
-            
-// LOOPS FOR EACH DAY IN CALENDAR
-for(index=0; index < DAYS_OF_MONTH; index++)
-{
-if( Calendar.getDate() > index )
-{
-// RETURNS THE NEXT DAY TO PRINT
-week_day =Calendar.getDay();
-            
-// START NEW ROW FOR FIRST DAY OF WEEK
-if(week_day == 0)
-cal += TR_start;
-            
-if(week_day != DAYS_OF_WEEK)
-{
-            
-// SET VARIABLE INSIDE LOOP FOR INCREMENTING PURPOSES
-var day  = Calendar.getDate();
-            
-// HIGHLIGHT TODAY'S DATE
-if( today==Calendar.getDate() )
-cal += highlight_start + day + highlight_end + TD_end;
-            
-// PRINTS DAY
-else
-cal += TD_start + day + TD_end;
-}
-            
-// END ROW FOR LAST DAY OF WEEK
-if(week_day == DAYS_OF_WEEK)
-cal += TR_end;
-}
-            
-// INCREMENTS UNTIL END OF THE MONTH
-Calendar.setDate(Calendar.getDate()+1);
-            
-}// end for loop
-            
-cal += '</TD></TR></TABLE></TABLE>';
-            
-//  PRINT CALENDAR
-document.write(cal);
-            
-//  End -->
-
+    var today = moment();
+  
+    function Calendar(selector, events) {
+      this.el = document.querySelector(selector);
+      this.events = events;
+      this.current = moment().date(1);
+      this.draw();
+      var current = document.querySelector('.today');
+      if(current) {
+        var self = this;
+        window.setTimeout(function() {
+          self.openDay(current);
+        }, 500);
+      }
+    }
+  
+    Calendar.prototype.draw = function() {
+      //Create Header
+      this.drawHeader();
+  
+      //Draw Month
+      this.drawMonth();
+  
+      this.drawLegend();
+    }
+  
+    Calendar.prototype.drawHeader = function() {
+      var self = this;
+      if(!this.header) {
+        //Create the header elements
+        this.header = createElement('div', 'header');
+        this.header.className = 'header';
+  
+        this.title = createElement('h1');
+  
+        var right = createElement('div', 'right');
+        right.addEventListener('click', function() { self.nextMonth(); });
+  
+        var left = createElement('div', 'left');
+        left.addEventListener('click', function() { self.prevMonth(); });
+  
+        //Append the Elements
+        this.header.appendChild(this.title); 
+        this.header.appendChild(right);
+        this.header.appendChild(left);
+        this.el.appendChild(this.header);
+      }
+  
+      this.title.innerHTML = this.current.format('MMMM YYYY');
+    }
+  
+    Calendar.prototype.drawMonth = function() {
+      var self = this;
+      
+      this.events.forEach(function(ev) {
+       ev.date = self.current.clone().date(Math.random() * (29 - 1) + 1);
+      });
+      
+      
+      if(this.month) {
+        this.oldMonth = this.month;
+        this.oldMonth.className = 'month out ' + (self.next ? 'next' : 'prev');
+        this.oldMonth.addEventListener('webkitAnimationEnd', function() {
+          self.oldMonth.parentNode.removeChild(self.oldMonth);
+          self.month = createElement('div', 'month');
+          self.backFill();
+          self.currentMonth();
+          self.fowardFill();
+          self.el.appendChild(self.month);
+          window.setTimeout(function() {
+            self.month.className = 'month in ' + (self.next ? 'next' : 'prev');
+          }, 16);
+        });
+      } else {
+          this.month = createElement('div', 'month');
+          this.el.appendChild(this.month);
+          this.backFill();
+          this.currentMonth();
+          this.fowardFill();
+          this.month.className = 'month new';
+      }
+    }
+  
+    Calendar.prototype.backFill = function() {
+      var clone = this.current.clone();
+      var dayOfWeek = clone.day();
+  
+      if(!dayOfWeek) { return; }
+  
+      clone.subtract('days', dayOfWeek+1);
+  
+      for(var i = dayOfWeek; i > 0 ; i--) {
+        this.drawDay(clone.add('days', 1));
+      }
+    }
+  
+    Calendar.prototype.fowardFill = function() {
+      var clone = this.current.clone().add('months', 1).subtract('days', 1);
+      var dayOfWeek = clone.day();
+  
+      if(dayOfWeek === 6) { return; }
+  
+      for(var i = dayOfWeek; i < 6 ; i++) {
+        this.drawDay(clone.add('days', 1));
+      }
+    }
+  
+    Calendar.prototype.currentMonth = function() {
+      var clone = this.current.clone();
+  
+      while(clone.month() === this.current.month()) {
+        this.drawDay(clone);
+        clone.add('days', 1);
+      }
+    }
+  
+    Calendar.prototype.getWeek = function(day) {
+      if(!this.week || day.day() === 0) {
+        this.week = createElement('div', 'week');
+        this.month.appendChild(this.week);
+      }
+    }
+  
+    Calendar.prototype.drawDay = function(day) {
+      var self = this;
+      this.getWeek(day);
+  
+      //Outer Day
+      var outer = createElement('div', this.getDayClass(day));
+      outer.addEventListener('click', function() {
+        self.openDay(this);
+      });
+  
+      //Day Name
+      var name = createElement('div', 'day-name', day.format('ddd'));
+  
+      //Day Number
+      var number = createElement('div', 'day-number', day.format('DD'));
+  
+  
+      //Events
+      var events = createElement('div', 'day-events');
+      this.drawEvents(day, events);
+  
+      outer.appendChild(name);
+      outer.appendChild(number);
+      outer.appendChild(events);
+      this.week.appendChild(outer);
+    }
+  
+    Calendar.prototype.drawEvents = function(day, element) {
+      if(day.month() === this.current.month()) {
+        var todaysEvents = this.events.reduce(function(memo, ev) {
+          if(ev.date.isSame(day, 'day')) {
+            memo.push(ev);
+          }
+          return memo;
+        }, []);
+  
+        todaysEvents.forEach(function(ev) {
+          var evSpan = createElement('span', ev.color);
+          element.appendChild(evSpan);
+        });
+      }
+    }
+  
+    Calendar.prototype.getDayClass = function(day) {
+      classes = ['day'];
+      if(day.month() !== this.current.month()) {
+        classes.push('other');
+      } else if (today.isSame(day, 'day')) {
+        classes.push('today');
+      }
+      return classes.join(' ');
+    }
+  
+    Calendar.prototype.openDay = function(el) {
+      var details, arrow;
+      var dayNumber = +el.querySelectorAll('.day-number')[0].innerText || +el.querySelectorAll('.day-number')[0].textContent;
+      var day = this.current.clone().date(dayNumber);
+  
+      var currentOpened = document.querySelector('.details');
+  
+      //Check to see if there is an open detais box on the current row
+      if(currentOpened && currentOpened.parentNode === el.parentNode) {
+        details = currentOpened;
+        arrow = document.querySelector('.arrow');
+      } else {
+        //Close the open events on differnt week row
+        //currentOpened && currentOpened.parentNode.removeChild(currentOpened);
+        if(currentOpened) {
+          currentOpened.addEventListener('webkitAnimationEnd', function() {
+            currentOpened.parentNode.removeChild(currentOpened);
+          });
+          currentOpened.addEventListener('oanimationend', function() {
+            currentOpened.parentNode.removeChild(currentOpened);
+          });
+          currentOpened.addEventListener('msAnimationEnd', function() {
+            currentOpened.parentNode.removeChild(currentOpened);
+          });
+          currentOpened.addEventListener('animationend', function() {
+            currentOpened.parentNode.removeChild(currentOpened);
+          });
+          currentOpened.className = 'details out';
+        }
+  
+        //Create the Details Container
+        details = createElement('div', 'details in');
+  
+        //Create the arrow
+        var arrow = createElement('div', 'arrow');
+  
+        //Create the event wrapper
+  
+        details.appendChild(arrow);
+        el.parentNode.appendChild(details);
+      }
+  
+      var todaysEvents = this.events.reduce(function(memo, ev) {
+        if(ev.date.isSame(day, 'day')) {
+          memo.push(ev);
+        }
+        return memo;
+      }, []);
+  
+      this.renderEvents(todaysEvents, details);
+  
+      arrow.style.left = el.offsetLeft - el.parentNode.offsetLeft + 27 + 'px';
+    }
+  
+    Calendar.prototype.renderEvents = function(events, ele) {
+      //Remove any events in the current details element
+      var currentWrapper = ele.querySelector('.events');
+      var wrapper = createElement('div', 'events in' + (currentWrapper ? ' new' : ''));
+  
+      events.forEach(function(ev) {
+        var div = createElement('div', 'event');
+        var square = createElement('div', 'event-category ' + ev.color);
+        var span = createElement('span', '', ev.eventName);
+  
+        div.appendChild(square);
+        div.appendChild(span);
+        wrapper.appendChild(div);
+      });
+  
+      if(!events.length) {
+        var div = createElement('div', 'event empty');
+        var span = createElement('span', '', 'No Events');
+  
+        div.appendChild(span);
+        wrapper.appendChild(div);
+      }
+  
+      if(currentWrapper) {
+        currentWrapper.className = 'events out';
+        currentWrapper.addEventListener('webkitAnimationEnd', function() {
+          currentWrapper.parentNode.removeChild(currentWrapper);
+          ele.appendChild(wrapper);
+        });
+        currentWrapper.addEventListener('oanimationend', function() {
+          currentWrapper.parentNode.removeChild(currentWrapper);
+          ele.appendChild(wrapper);
+        });
+        currentWrapper.addEventListener('msAnimationEnd', function() {
+          currentWrapper.parentNode.removeChild(currentWrapper);
+          ele.appendChild(wrapper);
+        });
+        currentWrapper.addEventListener('animationend', function() {
+          currentWrapper.parentNode.removeChild(currentWrapper);
+          ele.appendChild(wrapper);
+        });
+      } else {
+        ele.appendChild(wrapper);
+      }
+    }
+  
+    Calendar.prototype.drawLegend = function() {
+      var legend = createElement('div', 'legend');
+      var calendars = this.events.map(function(e) {
+        return e.calendar + '|' + e.color;
+      }).reduce(function(memo, e) {
+        if(memo.indexOf(e) === -1) {
+          memo.push(e);
+        }
+        return memo;
+      }, []).forEach(function(e) {
+        var parts = e.split('|');
+        var entry = createElement('span', 'entry ' +  parts[1], parts[0]);
+        legend.appendChild(entry);
+      });
+      this.el.appendChild(legend);
+    }
+  
+    Calendar.prototype.nextMonth = function() {
+      this.current.add('months', 1);
+      this.next = true;
+      this.draw();
+    }
+  
+    Calendar.prototype.prevMonth = function() {
+      this.current.subtract('months', 1);
+      this.next = false;
+      this.draw();
+    }
+  
+    window.Calendar = Calendar;
+  
+    function createElement(tagName, className, innerText) {
+      var ele = document.createElement(tagName);
+      if(className) {
+        ele.className = className;
+      }
+      if(innerText) {
+        ele.innderText = ele.textContent = innerText;
+      }
+      return ele;
+    }
+  }();
+  
+  !function() {
+    var data = [
+      { eventName: 'Lunch Meeting w/ Mark', calendar: 'Work', color: 'orange' },
+      { eventName: 'Interview - Jr. Web Developer', calendar: 'Work', color: 'orange' },
+      { eventName: 'Demo New App to the Board', calendar: 'Work', color: 'orange' },
+      { eventName: 'Dinner w/ Marketing', calendar: 'Work', color: 'orange' },
+  
+      { eventName: 'Game vs Portalnd', calendar: 'Sports', color: 'blue' },
+      { eventName: 'Game vs Houston', calendar: 'Sports', color: 'blue' },
+      { eventName: 'Game vs Denver', calendar: 'Sports', color: 'blue' },
+      { eventName: 'Game vs San Degio', calendar: 'Sports', color: 'blue' },
+  
+      { eventName: 'School Play', calendar: 'Kids', color: 'yellow' },
+      { eventName: 'Parent/Teacher Conference', calendar: 'Kids', color: 'yellow' },
+      { eventName: 'Pick up from Soccer Practice', calendar: 'Kids', color: 'yellow' },
+      { eventName: 'Ice Cream Night', calendar: 'Kids', color: 'yellow' },
+  
+      { eventName: 'Free Tamale Night', calendar: 'Other', color: 'green' },
+      { eventName: 'Bowling Team', calendar: 'Other', color: 'green' },
+      { eventName: 'Teach Kids to Code', calendar: 'Other', color: 'green' },
+      { eventName: 'Startup Weekend', calendar: 'Other', color: 'green' }
+    ];
+  
+    
+  
+    function addDate(ev) {
+      
+    }
+  
+    var calendar = new Calendar('#calendar', data);
+  
+  }();
